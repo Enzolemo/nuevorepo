f1710a401ad0a7d018fd9e811592edff8fbaa519
Enzolemo <enzolemo4@gmail.com>
2023-02-15 11:24:59 -0600

    rout

--- appReactPDC/src/App.jsx
+++ appReactPDC/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Menu from './componentes/Menu/Menu'
 import ComponenteContenedor from './componentes/ComponenteContainer/ComponenteContainer'
+import ItemDetailContainer from './componentes/itemDetailContainer/itemDetailContainer'
+import ItemCount from './componentes/ItemCount/ItemCount'
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
 
@@ -14,18 +16,20 @@ function App() {
   let count = 0
 
   return (
-    <>
+    <BrowserRouter>
 
       {/* Aca anido menu dentro de componente contenedor. entonces ahora menu es el children de componente contenedor; se podria decir que menu ahora es PROP de componente contenedor, en un prop llamado CHILDREN
       
       si paso solo un dato es un objeto (children) si paso mas de uno se hace un array de objetos, tambien llamado children*/}
-
+    
       <ComponenteContenedor saludo='Hola soy componente container'>
 
         <Menu />
 
       </ComponenteContenedor>
-    </>
+      <ItemDetailContainer />
+      <ItemCount initial={1} stock={10} onAdd = {()=>{}}/>
+    </BrowserRouter>
   )
 }
 
---
+++ appReactPDC/src/componentes/ItemCount/ItemCount.jsx
@@ -0,0 +1,7 @@
+const ItemCount = ({initial = 1, stock = 10, onAdd }) => {
+    return (
+        <div>ItemCount</div>
+    )
+}
+
+export default ItemCount
